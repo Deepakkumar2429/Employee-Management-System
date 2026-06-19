04877d8cdcbeb47801b488f6a2892bc02398f01a
DEEPAKKUMAR K <126558548+Deepakkumar2429@users.noreply.github.com>
2026-06-19 11:52:16 +0530

    Add files via upload

---
+++ employees/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.employee_list, name='employee_list'),
+    path('add/', views.employee_create, name='employee_add'),
+    path('delete/<int:id>/', views.employee_delete, name='employee_delete'),
+]
